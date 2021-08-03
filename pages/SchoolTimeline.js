d69efb755d19fa1cdc475a450c977af025ebc65c
Anglayrati <anglayrati@gmail.com>
2021-08-03 15:43:40 +0700

    fb

--- pages/SchoolTimeline.js
+++ pages/SchoolTimeline.js
@@ -3,6 +3,9 @@ import NavigationMenu from "./../components/containers/NavigationMenu"
 
 export default function SchoolTimeline(){
     return(
+      <div>
       <Appbar text="School Timeline"></Appbar>
+      <NavigationMenu/>
+      </div>
     )
 }

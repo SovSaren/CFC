91365bb8ecf5dd182828e0895134c862c58917f9
Anglayrati <anglayrati@gmail.com>
2021-08-04 16:06:44 +0700

    d

--- components/containers/NavigationMenu.js
+++ components/containers/NavigationMenu.js
@@ -2,7 +2,12 @@ import {Drawer} from '@material-ui/core'
 export default function NavigationMen(){
   return(
 <Drawer anchor="left" open={true}>
-<h1> Shcool </h1>
+<h1> Timeline </h1>
+<h1> Class Timeline </h1>
+<h1> Teaching Learning</h1>
+<h1> Reault Evaluation </h1>
+<h1> Attendance </h1>
+<h1> Permisson </h1>
 </Drawer>
   )
 }

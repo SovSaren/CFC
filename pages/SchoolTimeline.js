f94326118fbec84088a9a88e8974de5388eb4cbc
Anglayrati <anglayrati@gmail.com>
2021-08-04 16:24:48 +0700

    ffa

--- pages/SchoolTimeline.js
+++ pages/SchoolTimeline.js
@@ -1,11 +1,10 @@
 import Appbar from "./../components/containers/Appbar"
-import NavigationMenu from "./../components/containers/NavigationMenu"
 
 export default function SchoolTimeline(){
     return(
       <div>
       <Appbar text="School Timeline"></Appbar>
-      <NavigationMenu/>
+     
       </div>
     )
 }
